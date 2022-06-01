cc6ec4c2bf3258822dd50cf4dbf0a16eb9fc8a4f
Iamtunny <maestrotunexxxie@gmail.com>
2022-06-01 08:06:24 -0700

    my commit

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word}")
+print(f"First 3 letters: {word_first_3}")
 print(f"{word[:3]}")
-print(f"Last 2 letters: {word}")
+print(f"Last 2 letters: {word_last_2}")
 print(f"{word[-2:]}")
-print(f"Middle word: {word}")
+print(f"Middle word: {middle_word}")
 print(f"{word[1:8]}")
