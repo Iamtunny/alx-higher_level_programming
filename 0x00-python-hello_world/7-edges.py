ea736757cf9d422f0b4973fd2493d81ccf5134ac
Iamtunny <maestrotunexxxie@gmail.com>
2022-06-01 07:58:55 -0700

    my commit

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word}")
+print(f"{word[:3]}")
+print(f"Last 2 letters: {word}")
+print(f"{word[-2:]}")
+print(f"Middle word: {word}")
+print(f"{word[1:8]}")
