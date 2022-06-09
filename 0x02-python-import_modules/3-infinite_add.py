ec49842a17c1812ad19b113ab201c41ef560d954
Iamtunny <maestrotunexxxie@gmail.com>
2022-06-09 09:24:29 -0700

    my commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print("0")
     else:
         for j in range(1, number):
-            sum = sum + int(sys.argv[i])
+            sum = sum + int(sys.argv[j])
             print("{}".format(sum))
