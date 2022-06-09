24397c656f963d5bca1959beef3b979f37b4f363
Iamtunny <maestrotunexxxie@gmail.com>
2022-06-09 09:22:48 -0700

    my commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    number = len(sys.argv)
+    if number == 1:
+        print("0")
+    else:
+        for j in range(1, number):
+            sum = sum + int(sys.argv[i])
+            print("{}".format(sum))
